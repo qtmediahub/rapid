495206e89b93d9ed5d77eb471947dac292bccc29
Thomas Senyk <thomas.senyk@nokia.com>
2011-06-07 13:55:15 +0200

    6. 'Rearchitect' commit, fixing: cursor.js

--- 480/cursor.js
+++ 480/cursor.js
@@ -1,4 +1,7 @@
 function initialize() {
-    cursor.idleTimeout = 2
+    if (typeof(cursor) != 'undefined') {
+        cursor.idleTimeout = 2
+        cursor.defaultCursorPath = themeResourcePath + "/media/pointer-focus.png"
+        cursor.clickedCursorPath = themeResourcePath + "/media/pointer-focus-click.png"
+    }
 }
-
